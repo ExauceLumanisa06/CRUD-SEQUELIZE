bc6f2f7a7734d35856c3fc43ea2c5b5602f2e3e6
ExauceLumanisa06 <exaucelukifimpa06@gmail.com>
2023-10-14 15:06:41 +0200

    getAll du produit

--- controllers/produit.js
+++ controllers/produit.js
@@ -5,23 +5,46 @@ exports.createProduit = (req, res) => {
     const { pro_libelle, pro_prix } = req.body
     try {
         //verify if client exist
-        Produit.findOne({ where: { pro_libelle} })
+        Produit.findOne({ where: { pro_libelle } })
             .then(function (oldProduit) {
                 if (oldProduit) return res.status(309).json("Le produit existe déjà")
                 else Produit.create({
                     pro_libelle,
                     pro_prix
                 })
-                .then((produit) => {
-                    return res.status(201).json({ msg: "Le produit est crée", produit})
-    
-                })
-                .catch((err)=>res.json(err.message))
+                    .then((produit) => {
+                        return res.status(201).json({ msg: "Le produit est crée", produit })
+
+                    })
+                    .catch((err) => res.json(err.message))
             })
-     
+
 
     }
-     catch (error) {
+    catch (error) {
         res.json(error)
     }
+}
+
+exports.getAllProduit = (req, res) => {
+    try {
+        Produit.findAll()
+            .then((produits) => {
+                if (produits.length < 1) return res.status(404).send('Pas de produit in db')
+                else return res.status(200).json(produits)
+            })
+            .catch()
+    } catch (error) {
+        res.json(error)
+
+    }
+}
+exports.getOneProduit = (req, res) => {
+
+}
+exports.updateProduit = (req, res) => {
+
+}
+exports.deleteProduit = (req, res) => {
+
 }
--- routes/produit.js
+++ routes/produit.js
@@ -3,6 +3,7 @@ const router = Router()
 const ProduitControllers = require("../controllers/produit")
 
 router.post("/",ProduitControllers.createProduit)
+router.get("/",ProduitControllers.getAllProduit)
 
 
 
